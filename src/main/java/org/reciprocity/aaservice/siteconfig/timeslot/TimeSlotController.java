cd650fa812415b0b7685ac83d577753cf2693280
Antho <antmliu@gmail.com>
2020-06-17 23:46:09 -0400

    Adding some boilerplating

--- src/main/java/org/reciprocity/aaservice/siteconfig/timeslot/TimeSlotController.java
+++ src/main/java/org/reciprocity/aaservice/siteconfig/timeslot/TimeSlotController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class TimeslotController {
+public class TimeSlotController {
 
     @GetMapping(value="/timeslots")
     public ResponseEntity<List<TimeSlot>> getAllTimeSlots() {
@@ -20,5 +20,15 @@ public class TimeslotController {
     @PostMapping(value="/timeslot")
     public ResponseEntity<String> createTimeSlot(@RequestBody TimeSlotRequest request) {
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/timeslot")
+    public ResponseEntity<String> updateTimeSlot(@RequestBody TimeSlot request) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/timeslot/{id}")
+    public ResponseEntity<String> deleteTimeSlot(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
